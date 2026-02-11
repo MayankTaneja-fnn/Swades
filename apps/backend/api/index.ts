bec17d9876f21f1c5308aacfecfe580aedba228a
MayankTaneja-fnn <tanejamayank21@google.com>
2026-02-11 23:20:31 +0530

    updated index.ts

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,8 +1,8 @@
-import { handle } from 'hono/vercel';
-import { app } from '../src/app.js';
+import { handle } from "hono/vercel";
+import { app } from "../src/app.js";
 
 export const config = {
-    runtime: 'edge',
+  runtime: "nodejs",
 };
 
-// export default handle(app);
+export default handle(app);
